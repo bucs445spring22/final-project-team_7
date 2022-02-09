b313c7a25582eaf19a1c2e9faea2789b52caaf78
Andrew-Goetz <agoetz3@binghamton.edu>
2022-02-09 12:12:38 -0500

    created main

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    team = {
+         "Team Member 1": "Andrew Goetz", 
+         "Team Member 2": "Jin Xian Li",
+         "Team Member 3": "Yong Liu",
+    }
+
+if __name__ == "__main__":
+    main()
