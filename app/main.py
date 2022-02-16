d1472b7fd25d45dba8eda3a2baf27c768b90b799
root <root@DESKTOP-BI0BBI9.localdomain>
2022-02-16 12:00:27 -0500

    flask added

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
+from flask import *
+app = Flask(__name__)
+
+@app.route('/')
+def render():
+    print("Hello world")
+
 def main():
+    app.run()
     team = {
          "Team Member 1": "Andrew Goetz", 
          "Team Member 2": "Jin Xian Li",
          "Team Member 3": "Yong Liu",
     }
-    print("Hello World")
 
 if __name__ == "__main__":
     main()
