2fdab7e4ab7fae4ed702b3769107645a450ea7fd
jli452 <jli452@binghamton.edu>
2022-03-02 12:28:01 -0500

    added MediaEntry class

---
+++ app/MediaEntry.py
@@ -0,0 +1,9 @@
+class MediaEntry:
+	def __init__(self, media_type, genres, rating, cover_art, actors, release_date, content_rating):
+		this.media_type = media_type
+		this.genres = genres
+		this.rating = rating #User rating can be added later
+		this.cover_art = cover_art
+		this.actors = actors
+		this.release_date = release_date
+		this.content_rating = content_rating
