bf0c8ea68e32df55bad52b6d6b883b9bc5ab1b9e
jli452 <jli452@binghamton.edu>
2022-03-09 12:45:06 -0500

    added html templates for login and signup

--- app/app.py
+++ app/app.py
@@ -2,8 +2,27 @@ from flask import *
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'woohoo'
 
-@app.route('/')
+@app.route('/', methods=('GET', 'POST'))
 def render():
+    error = None
+    if request.method == 'POST' and 'login' in request.form:
+        return redirect(url_for('login'))
+    elif request.method == 'POST' and 'signup' in request.form:
+        return redirect(url_for('signup'))
+    return render_template('index.html')
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('signup'))
+    return render_template('login.html', error=error)
+
+@app.route('/signup', methods=('GET', 'POST'))
+def signup():
     return "Hello world"
 
 def main():
