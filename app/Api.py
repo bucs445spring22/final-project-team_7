40a00c28d5801f11f2d2a89bd0975dd76ba6b0b6
Andrew Goetz <agoetz3@binghamton.edu>
2022-03-21 10:57:40 -0400

    api and tmdb_api classes

---
+++ app/Api.py
@@ -0,0 +1,11 @@
+class Api:
+    API_KEY = ""
+    LANGUAGE = "en"
+    def __init__(self):
+        pass
+    def recommend(self, media_id):
+        pass
+    def search_by_id(self, media_id):
+        pass
+    def search_by_title(self, title):
+        pass
---
+++ app/Tmdb_api.py
@@ -0,0 +1,21 @@
+from tmdbv3api import Movie, TMDb
+
+class Tmdb_api(Api):
+    API_KEY = "d7dbb644708bdabf2a395267c0890814"
+    LANGUAGE = "en-US"
+    COVER_URL = "https://www.themoviedb.org/t/p/w1280"
+    def __init__(self):
+        pass
+    def get_movie(self, media_id):
+        movie = Movie()
+        m = movie.details(media_id)
+        cover = COVER_URL + m.poster_path
+        pass # should return a MovieEntry?
+    def get_show(self, media_id):
+        pass
+    def recommend(self, media_id):
+        pass
+    def search_by_id(self, media_id):
+        pass
+    def search_by_title(self, title):
+        pass
