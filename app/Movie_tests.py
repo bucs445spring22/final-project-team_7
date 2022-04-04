f18ffa570b0159a2e39bfa4f56e0a6cc00cb24fc
Andrew-Goetz <agoetz3@binghamton.edu>
2022-04-04 12:39:24 -0400

    added more tests

--- app/Movie_tests.py
+++ app/Movie_tests.py
@@ -2,7 +2,7 @@ from Movie import Movie
 import pytest
 
 def test_verify_constructor():
-    x = Movie(10, "Test", "overview", "1984", 9, "/url.jpg")
+    x = Movie(10, "Test", "overview", "1984", 9, "https://www.themoviedb.org/t/p/w188_and_h282_bestv2/url.jpg")
     assert x.media_id == 10
     assert x.title == "Test"
     assert x.overview == "overview"
--- app/Tmdb_api.py
+++ app/Tmdb_api.py
@@ -6,26 +6,30 @@ import json
 class Tmdb_api:
     API_KEY = "d7dbb644708bdabf2a395267c0890814"
 
-    def __request_to_dict(self, url):
+    def request_to_dict(self, url):
         return json.loads(requests.get(url).text)
 
-    def __thumbnail_gen(self, poster_path):
+    def thumbnail_gen(self, poster_path):
         if str(poster_path) == "None":
             return "https://blog.springshare.com/wp-content/uploads/2010/02/nc-md.gif"
-        return "https://www.themoviedb.org/t/p/w188_and_h282_bestv2" + str(poster_path)
+        return "https://www.themoviedb.org/t/p/w188_and_h282_bestv2" + poster_path
 
-    def __cover_gen(self, poster_path):
+    def cover_gen(self, poster_path):
         if str(poster_path) == "None":
             return "https://blog.springshare.com/wp-content/uploads/2010/02/nc-md.gif"
-        return "https://www.themoviedb.org/t/p/w1280" + str(poster_path)
+        return "https://www.themoviedb.org/t/p/w1280" + poster_path
+
+    def date_gen(self, date):
+        #YYYY-MM-DD to YYYY
+        return date[0:4]
 
     def get_movie(self, media_id):
-        data = self.__request_to_dict("https://api.themoviedb.org/3/movie/" + str(media_id) + "?api_key=" + self.API_KEY)
-        movie = Movie(data.get('id'), data.get('title'), data.get('overview'), data.get('release_date'), data.get('vote_average'), self.__thumbnail_gen(data.get('poster_path')))
+        data = self.request_to_dict("https://api.themoviedb.org/3/movie/" + str(media_id) + "?api_key=" + self.API_KEY)
+        movie = Movie(data.get('id'), data.get('title'), data.get('overview'), self.date_gen(data.get('release_date')), data.get('vote_average'), self.thumbnail_gen(data.get('poster_path')))
         movie.runtime = data.get('runtime')
         movie.language = data.get('original_language')
         movie.genres = data.get('genres')
-        movie.cover_url = self.__cover_gen(data.get('poster_path'))
+        movie.cover_url = self.cover_gen(data.get('poster_path'))
         return movie
 
     def get_show(self, media_id):
@@ -36,9 +40,9 @@ class Tmdb_api:
 
     def search(self, title):
         title = title.replace(" ", "+")
-        movie_data = self.__request_to_dict("https://api.themoviedb.org/3/search/movie?api_key=" + self.API_KEY + "&query=" + title)
-        #show_data = self.__request_to_dict("https://api.themoviedb.org/3/search/tv?api_key=" + self.API_KEY + "&query=" + title)
+        movie_data = self.request_to_dict("https://api.themoviedb.org/3/search/movie?api_key=" + self.API_KEY + "&query=" + title)
+        #show_data = self.request_to_dict("https://api.themoviedb.org/3/search/tv?api_key=" + self.API_KEY + "&query=" + title)
         ret = []
         for i in movie_data.get('results'):
-            ret.append(Movie(i.get('id'), i.get('title'), i.get('overview'), i.get('release_date'), i.get('vote_average'), self.__thumbnail_gen(i.get('poster_path'))))
+            ret.append(Movie(i.get('id'), i.get('title'), i.get('overview'), self.date_gen(i.get('release_date')), i.get('vote_average'), self.thumbnail_gen(i.get('poster_path'))))
         return ret
