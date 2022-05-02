f0e8abff5200bb6270cba438657a737845a628a3
Andrew-Goetz <agoetz3@binghamton.edu>
2022-05-02 12:42:50 -0400

    added documentation, very important

--- app/Api.py
+++ app/Api.py
@@ -1,8 +1,17 @@
 class Api:
     API_KEY = ""
     def __init__(self):
+        """
+        Constructor which constructs a Api construct.
+        """
         pass
     def recommend(self, media_id):
+        """
+        Template class for Apis
+        """
         pass
     def search(self, title):
+        """
+        Template class for Apis
+        """
         pass
