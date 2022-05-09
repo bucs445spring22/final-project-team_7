380c69c0eef35b7efe650c336d8185f9ed97b9a4
Yong Liu <yongliu1212@gmail.com>
2022-05-09 10:50:14 -0400

    db folder

--- db/app.py
+++ db/app.py
@@ -1,7 +1,9 @@
 import contextlib
-from loginDB.loginDB import loginDB
+from model.user import User
+from model.db import Database
 from tinydb import TinyDB, Query
 import os
+import json
 from flask import Flask, request
 
 
@@ -12,6 +14,42 @@ app.config.from_object('config.Config')
 with contextlib.suppress(OSError):
     os.makedirs(app.instance_path)
 
-@app.route('/users', methods=('get',))
+@app.route('/users', methods=('GET',))
 def index():
     return loginDB.all()
+
+@app.route('/verifySignUp', methods=('POST',))
+def verifySignUp():
+    print(f'FORM FOR SIGN UP: {request.json}')
+    print("DEBUGGER: In app.py verifySignup()")
+    name = request.json.get('username')
+    password = request.json.get('password')
+    u = User(name, password)
+    return u.add_user()
+
+@app.route('/verifyLogin', methods=('POST',))
+def verify():
+    print(f'FORM: {request.json}')
+    print("DEBUGGER: In app.py verify()")
+    name = request.json.get('username')
+    password = request.json.get('password')
+    u = User(name, password)
+    return u.verify_login()
+
+@app.route('/lookup_library', methods=('POST',))#return db
+def lookup_library():
+    name = request.json.get('user')
+    datab = Database(name)
+    return datab.lookup_library()
+
+@app.route('/add_movie', methods=('POST',))
+def add_movie():
+    name = request.json.get('username')
+    print("USER WE ARE ADDING TO: ",name)
+    data = request.json.get('data')
+    datab = Database(name)
+    return datab.add_movie(data)
+
+@app.route('/lookup_media', methods=('POST',))
+def lookup_media():
+    pass
---
+++ db/model/db.py
@@ -0,0 +1,33 @@
+from tinydb import TinyDB, Query, table
+#from tinydb.operations import set, Delete
+import json
+
+class Database:
+    def __init__(self, username = ""):
+        self.username = username
+        self.string = username + "_DB.json"
+        self.db = TinyDB(self.string)
+       
+    
+    def lookup_library(self):
+        with open(self.string) as json_file:
+            data = json.load(json_file)
+        return data
+
+    def add_movie(self,data) -> bool:
+        db = TinyDB(self.string)
+        que = Query()
+
+        # if db.all()[0].doc_id == 0.0:
+        #     if db.all()[0].get("title") == "Example":
+
+
+        for i in range(len(db.all())):
+            if db.all()[i].doc_id == data:
+                return {"Results": False and "Already in library"}
+        
+        db.insert(table.Document((data), doc_id = data.get("media_id")))
+
+        return {"Results": True and "Added to library"}
+                
+
