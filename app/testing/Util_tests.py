386f83b925e8fb843738b490761dae02b02475c0
Andrew-Goetz <agoetz3@binghamton.edu>
2022-05-12 22:23:15 -0400

    update tests, added user_tests.py to test User in db

--- app/testing/Util_tests.py
+++ app/testing/Util_tests.py
@@ -1,11 +1,8 @@
 import sys
 sys.path.insert(0, '..')
 from Tmdb_api import Tmdb_api
-from Util import request_to_dict, library_search, build_data, build_media
+from Util import request_to_dict, library_search
 import pytest
-#from tinydb import TinyDB, Query
-#from tinydb.operations import add
-#from tinydb.operations import delete, set
 
 def test_request_to_dict():
     api = Tmdb_api()
@@ -15,9 +12,3 @@ def test_library_search():
     media_list = ["test", "TEST", "best", "Star Wars"]
     assert(library_search(media_list, "test") == ["test", "TEST"])
     assert(library_search(media_list, "Star Wars") == ["Star Wars"])
-
-def test_build_data():
-    pass
-
-def test_build_media():
-    pass
